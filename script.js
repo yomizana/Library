cd570307607af66ba0b5c38c26d47894c27cf9b1
Tomas 'Yomi' Caraballo <yomizana@gmail.com>
2023-02-07 20:30:35 -0400

    Add function to add books to library

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-let myLibrary = ["The Hobbit", "The Thing", "A Clockwork Orange"];
+const myLibrary = [];
 const showFormButton = document.getElementById("show-form-btn");
-const addBookForm = document.getElementById("add-book-form");
+const bookForm = document.getElementById("book-form");
+const addBookButton = document.getElementById("add-book-btn");
 
 showFormButton.addEventListener("click", () => {
-  addBookForm.classList.toggle("hidden");
+  bookForm.classList.toggle("hidden");
+
+  if (showFormButton.textContent === "Show Form") {
+    showFormButton.textContent = "Hide Form";
+  } else {
+    showFormButton.textContent = "Show Form";
+  }
 });
 
-function Book() {
-  // the constructor...
+class Book {
+  constructor(title, year) {
+    this.title = title;
+    this.year = year;
+  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-function displayBooks() {
+function displayBooks(newBook) {
   const tableBody = document.getElementById("table-body");
+  const libraryLength = myLibrary.length;
 
-  myLibrary.forEach((book) => {
-    const newRow = document.createElement("tr");
-    const newColumn = document.createElement("td");
-    newColumn.textContent = book;
-    tableBody.appendChild(newRow);
-    newRow.appendChild(newColumn);
-  });
+  myLibrary.push(newBook);
+
+  const row = tableBody.insertRow();
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  cell1.textContent = myLibrary[libraryLength].title;
+  cell2.textContent = myLibrary[libraryLength].year;
 }
 
-displayBooks();
+function addBookToLibrary(title, year) {
+  const newBook = new Book(title, year);
+  displayBooks(newBook);
+}
+
+addBookButton.addEventListener("click", () => {
+  const title = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+  addBookToLibrary(title, year);
+});
